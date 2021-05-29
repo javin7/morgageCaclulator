f6a03ef9a2be23c3c4b080a6d794d9b758c0a42e
Javin <47981364+javin7@users.noreply.github.com>
2021-05-29 08:13:38 -0400

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,24 +11,29 @@ public class Main {
         //Scanner to take user input
         Scanner in = new Scanner(System.in);
 
-        //Welcome
+        //Welcome message
         System.out.print("WELCOME TO THE MORTGAGE ESTIMATE CALCULATOR!\nGET AN ESTIMATE ON THE COSTS OF GETTING A NEW HOME!\n");
 
         //Get principal loan amount
         System.out.print("Enter the principal loan amount: $");
         double principalLoanAmount = in.nextDouble();
+
         //Get monthly interest rate
         System.out.print("Enter your interest rate: %");
         double monthlyInterestRate = in.nextDouble();
+
         //Get months of payment
         System.out.print("How many months of payment are over the loans lifetime: ");
         double numberOfPayments = in.nextDouble();
+
         //Get how much is down payment
         System.out.print("What percentage is the down payment: %");
         double downPayment = in.nextDouble();
+
         //Get property tax
         System.out.print("Property tax: $");
         double propertyTax = in.nextDouble();
+
         //Get HOA fees
         System.out.print("HOA fees: $");
         double HOAFees = in.nextDouble();
@@ -37,7 +42,9 @@ public class Main {
         monthlyInterestRate = (monthlyInterestRate / 100) / 12;
 
         //Calculate total month mortgage payment
-        double totalMonthlyMortgagePayment = (principalLoanAmount * (1 -(downPayment / 100))) * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate),numberOfPayments)) / (Math.pow((1 + monthlyInterestRate),numberOfPayments)-1)));
+        double totalMonthlyMortgagePayment = (principalLoanAmount * (1 -(downPayment / 100)))
+                * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate),numberOfPayments))
+                / (Math.pow((1 + monthlyInterestRate),numberOfPayments)-1)));
 
         //Round numbers down to 2 decimals
         totalMonthlyMortgagePayment = (Math.round(totalMonthlyMortgagePayment * 100)) / 100;
